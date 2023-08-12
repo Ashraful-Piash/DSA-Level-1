1e9160430f0cc731242feb3167c213366ddb95d1
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-08-12 15:49:13 +0600

    Updated

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("*\n**\n***\n****\n*****");
         System.out.println("*****\n   *\n  *\n *\n*****");
+
+
+        int n1 = 15;
+        int n2 = 10;
+
+        if (n1 == n2) {
+
+            System.out.println(n1 +" equals to " + n2);
+        }
+        else{
+            if(n1>n2){
+                System.out.println(n1 +" is greater than "+n2);
+            }else {
+                System.out.println(n1 + " is smaller than "+n2);
+            }
+        }
     }
 }
